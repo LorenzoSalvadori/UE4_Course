febf4d2c37138d28be6be5615a4ec73fb85e31db
Lorenzo Salvadori <lorenzo.salvadori.86@gmail.com>
2016-10-17 03:46:46 +0200

    encapsulation and abstraction

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -2,26 +2,63 @@
 #include<string>
 
 using namespace std;
+void PrintIntro();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 
+// the entry point for our application
 int main()
 {
-	// introduce the game
+	do {
+		PrintIntro();
+		PlayGame();
+	}
+	while (AskToPlayAgain());
+
+	return 0; // exit the application
+}
+
+// introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENGTH = 9;
-	
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
+string GetGuess()
+{
 	// get a guess from the user
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	cout << endl;
 
-	// repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-	
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again (y/n)?";
+	string Response = "";
+	getline(cin, Response);
 	cout << endl;
-	return 0;
+	
+	return (Response[0] == 'y' || Response[0] == 'Y');
+}
+
+void PlayGame() 
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 0; i < NUMBER_OF_TURNS; i++)
+	{
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+	return;
 }
